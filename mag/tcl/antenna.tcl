dd319d6b9ed3f9456b9ddd3a31209da61938c9a0
abhilash0419 <abhilash.cccir@cambridge.edu.in>
2026-03-12 12:30:15 +0530

    Add files via upload

--- mag/tcl/antenna.tcl
+++ mag/tcl/antenna.tcl
@@ -1,8 +1,7 @@
 set project [lindex $argv $argc-1]
 load $project.mag
 select top cell
-drc euclidean on
-drc style drc(full)
-drc check
-set drcresult [drc listall why]
+extract all
+antennacheck debug
+antennacheck
 quit -noprompt
